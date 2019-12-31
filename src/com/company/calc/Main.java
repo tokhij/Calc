277e66bbbd89da998dd64361fa52844b5e1b2801
John Tokhi <56596400+tokhij@users.noreply.github.com>
2019-12-31 00:10:33 -0500

    added math operators, runs

--- src/com/company/calc/Main.java
+++ src/com/company/calc/Main.java
@@ -3,6 +3,28 @@ package com.company.calc;
 public class Main {
 
     public static void main(String[] args) {
+        double val1 = 100.0;
+        double val2 = 50.0;
+        double result;
+        char opCode = 'd';
 
+        if(opCode == 'a')
+            result = val1 + val2;
+        else if(opCode == 's')
+            result = val1 - val2;
+        else if(opCode == 'd')
+            result = val2 != 0.0 ? val1 / val2 : 0.0;
+// ternary operator replaces the next 4 lines
+//            if(val2 != 0.0)
+//                result = val1 / val2;
+//            else
+//                result = 0.0;
+        else if(opCode == 'm')
+            result = val1 * val2;
+        else {
+            System.out.println("Error - invalid opCode");
+            result = 0.0;
+        }
+        System.out.println(result);
     }
 }
