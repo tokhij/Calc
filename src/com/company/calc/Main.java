bada1087c91c7eb07f8a2de10e7ebab83829aba6
John Tokhi <56596400+tokhij@users.noreply.github.com>
2019-12-31 00:40:47 -0500

    adding classes

--- src/com/company/calc/Main.java
+++ src/com/company/calc/Main.java
@@ -3,43 +3,34 @@ package com.company.calc;
 public class Main {
 
     public static void main(String[] args) {
-        double[] leftVal = {100.0, 25.0, 225.0, 11.0};
-        double[] rightVal = {50.0, 92.0, 17.0, 3.0};
-        char[] opCodes = {'d', 'a', 's', 'm'};
-        double[] results = new double[opCodes.length];
+//        double[] leftVal = {100.0, 25.0, 225.0, 11.0};
+//        double[] rightVal = {50.0, 92.0, 17.0, 3.0};
+//        char[] opCodes = {'d', 'a', 's', 'm'};
+//        double[] results = new double[opCodes.length];
 //        double val1 = 100.0;
 //        double val2 = 50.0;
 //        double result;
 //        char opCode = 'd';
 
-        for(int i = 0; i < opCodes.length; i++) {
-            switch(opCodes[i]){
-                case 'a':
-                    results[i] = leftVal[i] + rightVal[i];
-                    break;
-                case 's':
-                    results[i] = leftVal[i] - rightVal[i];
-                    break;
-                case 'd':
-                    results[i] = rightVal[i] != 0.0 ? leftVal[i] / rightVal[i] : 0.0;
-                    // ternary operator replaces the next 4 lines
-                    //            if(val2 != 0.0)
-                    //                result = val1 / val2;
-                    //            else
-                    //                result = 0.0;
-                    break;
-                case 'm':
-                    results[i] = leftVal[i] * rightVal[i];
-                    break;
-                default:
-                    System.out.println("Error - invalid opCode");
-                    results[i] = 0.0;
-                    break;
-            }
-        }
-        for (double theResult : results) {
+        MathEquation[] equations = new MathEquation[4];
+        equations[0] = create(100.0, 50.0, 'd');
+        equations[1] =  create(25.0, 92.0, 'a');
+        equations[2] =  create(225.0, 17.0, 's');
+        equations[3] =  create(11.0, 3.0, 'm');
+        for (MathEquation equation: equations) {
+            equation.execute();
             System.out.print("result = ");
-            System.out.println(theResult);
+            System.out.println(equation.result);
         }
+    }
+
+
+    public static MathEquation create(double leftVal, double rightVal, char opCode){
+        MathEquation equation = new MathEquation();
+        equation.leftVal = leftVal;
+        equation.rightVal = rightVal;
+        equation.opCode = opCode;
+
+        return equation;
     }
 }
--- src/com/company/calc/MathEquation.java
+++ src/com/company/calc/MathEquation.java
@@ -1,4 +1,37 @@
 package com.company.calc;
 
 public class MathEquation {
+    public double leftVal;
+    public double rightVal;
+    public char opCode;
+    public double result;
+
+
+    public void execute() {
+        switch(opCode){
+            case 'a':
+                result = leftVal + rightVal;
+                break;
+            case 's':
+                result = leftVal - rightVal;
+                break;
+            case 'd':
+                result = rightVal != 0.0 ? leftVal / rightVal : 0.0;
+                // ternary operator replaces the next 4 lines
+                //            if(val2 != 0.0)
+                //                result = val1 / val2;
+                //            else
+                //                result = 0.0;
+                break;
+            case 'm':
+                result = leftVal * rightVal;
+                break;
+            default:
+                System.out.println("Error - invalid opCode");
+                result = 0.0;
+                break;
+        }
+    }
+
+
 }
