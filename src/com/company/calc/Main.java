88338dfdafaa7b3dbb9947c12d069df8d76952e0
John Tokhi <56596400+tokhij@users.noreply.github.com>
2019-12-31 00:27:32 -0500

    cleaned up math operators

--- src/com/company/calc/Main.java
+++ src/com/company/calc/Main.java
@@ -3,28 +3,43 @@ package com.company.calc;
 public class Main {
 
     public static void main(String[] args) {
-        double val1 = 100.0;
-        double val2 = 50.0;
-        double result;
-        char opCode = 'd';
+        double[] leftVal = {100.0, 25.0, 225.0, 11.0};
+        double[] rightVal = {50.0, 92.0, 17.0, 3.0};
+        char[] opCodes = {'d', 'a', 's', 'm'};
+        double[] results = new double[opCodes.length];
+//        double val1 = 100.0;
+//        double val2 = 50.0;
+//        double result;
+//        char opCode = 'd';
 
-        if(opCode == 'a')
-            result = val1 + val2;
-        else if(opCode == 's')
-            result = val1 - val2;
-        else if(opCode == 'd')
-            result = val2 != 0.0 ? val1 / val2 : 0.0;
-// ternary operator replaces the next 4 lines
-//            if(val2 != 0.0)
-//                result = val1 / val2;
-//            else
-//                result = 0.0;
-        else if(opCode == 'm')
-            result = val1 * val2;
-        else {
-            System.out.println("Error - invalid opCode");
-            result = 0.0;
+        for(int i = 0; i < opCodes.length; i++) {
+            switch(opCodes[i]){
+                case 'a':
+                    results[i] = leftVal[i] + rightVal[i];
+                    break;
+                case 's':
+                    results[i] = leftVal[i] - rightVal[i];
+                    break;
+                case 'd':
+                    results[i] = rightVal[i] != 0.0 ? leftVal[i] / rightVal[i] : 0.0;
+                    // ternary operator replaces the next 4 lines
+                    //            if(val2 != 0.0)
+                    //                result = val1 / val2;
+                    //            else
+                    //                result = 0.0;
+                    break;
+                case 'm':
+                    results[i] = leftVal[i] * rightVal[i];
+                    break;
+                default:
+                    System.out.println("Error - invalid opCode");
+                    results[i] = 0.0;
+                    break;
+            }
         }
-        System.out.println(result);
+        for (double theResult : results) {
+            System.out.print("result = ");
+            System.out.println(theResult);
+        }
     }
 }
---
+++ src/com/company/calc/MathEquation.java
@@ -0,0 +1,4 @@
+package com.company.calc;
+
+public class MathEquation {
+}
